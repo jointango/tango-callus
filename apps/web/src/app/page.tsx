d7accbdc2b1eaccd5a6ee685f2d4ca14d6e7eac1
fahyik <fahyik@jointango.fr>
2024-07-23 11:07:22 +0200

    day 3

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,67 +1,40 @@
 "use client";
 
-import { useEffect, useState, ChangeEvent, FormEvent } from "react";
-import { Button } from "@repo/ui/button";
+import Link from "next/link";
+import { useState } from "react";
 
-const API_HOST = process.env.NEXT_PUBLIC_API_HOST || "http://localhost:3001";
+const listOfBrands = [
+  "adidas",
+  "nike",
+  "puma",
+  "reebok",
+  "under armour",
+  "hummels",
+];
 
 export default function Web() {
-  const [name, setName] = useState<string>("");
-  const [response, setResponse] = useState<{ message: string } | null>(null);
-  const [error, setError] = useState<string | undefined>();
+  return (
+    <div>
+      {listOfBrands.map((brand) => (
+        <MyComponent title={brand}></MyComponent>
+      ))}
+    </div>
+  );
+}
 
-  useEffect(() => {
-    setResponse(null);
-    setError(undefined);
-  }, [name]);
+function MyComponent(props: { title: string }) {
+  const [likes, setLikes] = useState(0);
 
-  const onChange = (e: ChangeEvent<HTMLInputElement>) =>
-    setName(e.target.value);
-
-  const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    try {
-      const result = await fetch(`${API_HOST}/message/${name}`);
-      const response = await result.json();
-      setResponse(response);
-    } catch (err) {
-      console.error(err);
-      setError("Unable to fetch response");
-    }
-  };
-
-  const onReset = () => {
-    setName("");
-  };
+  function handleOnClick() {
+    setLikes(likes + 1);
+  }
 
   return (
     <div>
-      <h1>Web</h1>
-      <form onSubmit={onSubmit}>
-        <label htmlFor="name">Name </label>
-        <input
-          type="text"
-          name="name"
-          id="name"
-          value={name}
-          onChange={onChange}
-        ></input>
-        <Button type="submit">Submit</Button>
-      </form>
-      {error && (
-        <div>
-          <h3>Error</h3>
-          <p>{error}</p>
-        </div>
-      )}
-      {response && (
-        <div>
-          <h3>Greeting</h3>
-          <p>{response.message}</p>
-          <Button onClick={onReset}>Reset</Button>
-        </div>
-      )}
+      <h1>{props.title}</h1>
+
+      <h2>{likes} ❤️</h2>
+      <button onClick={handleOnClick}>Click to like</button>
     </div>
   );
 }
